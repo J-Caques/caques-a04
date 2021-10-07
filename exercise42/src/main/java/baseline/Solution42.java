97d9ef26819500f07305f4b1dadaa9bcf5727066
J-Caques <j.caques@knights.ucf.edu>
2021-10-07 15:18:01 -0400

    pseudocode

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,16 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Caques
+ */
+
+/*
+Construct method that reads in data file exercise42_input.txt
+Construct method to parse data
+    -Process the records into the last name, first name, and salary of an individual
+Construct Method that creates a table
+    -Display results with a column for each of the three categories
+ */
+
+public class Solution42 {
+}
