79da40771be8d2be889997091c88d6e1e78b7910
J-Caques <j.caques@knights.ucf.edu>
2021-10-06 22:59:00 -0400

    Pseudocode & PlantUML Diagram

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,18 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Caques
+ */
+
+/*
+
+    Create method to read exercise41_input.txt
+        Extract list of names from file
+    Create method toArrange names alphabetically
+    Create method to write to exercise41_output.txt
+        Display total # of names followed by alphabetized list
+ */
+
+public class Solution41 {
+
+}
