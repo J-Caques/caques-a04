25f7c1f7c67ac3a0ba988ba96be213ac4335c049
J-Caques <j.caques@knights.ucf.edu>
2021-10-07 14:58:55 -0400

    code written, minor alterations to pseudo code and plantUML diagram

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -6,23 +6,84 @@ package baseline;
 
 /*
 
-    Create method to read exercise41_input.txt
-        Extract list of names from file
-    Create method toArrange names alphabetically
-    Create method to write to exercise41_output.txt
-        Display total # of names followed by alphabetized list
+
+
+
+
+
  */
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Solution41 {
-    private void readFile(){
+    public static void main(String[] args) {
+        String[] names = new String[7];
+        Solution41 object = new Solution41();
+        object.readFile(names);
+        object.order(names);
+        object.writeFile(names);
+
 
     }
+    //Create method to read exercise41_input.txt
+    private void readFile(String[] names){
 
-    private void order(){
+        //Extract list of names from file
+        try(Scanner input = new Scanner(Paths.get("data/exercise41_input.txt"))){
 
+            //Alter names[] by inputting names from file
+            if(input.hasNextLine()){
+                for(int i = 0; i < names.length; i++)
+                    names[i] = input.nextLine();
+            }
+        }
+        catch (IOException | NoSuchElementException | IllegalStateException e){
+            e.printStackTrace();
+        }
     }
 
-    private void writeFile(){
+    //Create method to arrange names alphabetically
+    private void order(String [] names){
+
+        //Go down list of names in names[]
+        for (int i = 0; i < names.length; i++) {
+            for (int j = i + 1; j < names.length; j++) {
+
+                // use compareTo to order strings in alphabetical order
+                if (names[i].compareTo(names[j]) > 0) {
+                    // swapping
+                    String temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+                }
+            }
+        }
+    }
+
+    //Create method to write to exercise41_output.txt
+    private void writeFile(String [] names){
+        try {
+            PrintWriter pw = new PrintWriter("data/exercise41_output.txt");
+
+            // Display total # of names followed by alphabetized list
+            pw.printf("Total of %d, names%n", names.length);
+            for(int i = 0; i < 18; i++){
+                pw.print("-");
+            }
+            pw.printf("%n");
+            for(String name : names){
+                pw.println(name);
+            }
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- exercise41/src/test/java/baseline/Solution41Test.java
+++ exercise41/src/test/java/baseline/Solution41Test.java
@@ -2,7 +2,6 @@ package baseline;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
