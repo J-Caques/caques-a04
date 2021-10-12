322fb68efeb7140384b97a11727338f219ee6340
J-Caques <j.caques@knights.ucf.edu>
2021-10-12 12:26:29 -0400

    Initial pseudocode w/ plantUML diagram

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,26 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Caques
+ */
+
+/*
+    Prompt User for:
+        Site name:
+        Author:
+    Ask User:
+        "Do you want a folder for JavaScript (y/n)?:"
+        "Do you want a folder for CSS (y/n)?:"
+    Generate index.html file that contains:
+        - site name inside <title> tag
+        - author inside <meta> tag
+    Output:
+        Created ./website/site_name
+        Created ./website/site_name/index.html
+        Created ./website/site_name/js/
+        Created ./website/site_name/css/
+ */
+
+public class Solution43 {
+}
