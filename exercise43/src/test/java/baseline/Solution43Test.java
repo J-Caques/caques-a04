84eeea09241d0b96df4134a29315c1b56061d873
J-Caques <j.caques@knights.ucf.edu>
2021-10-12 13:50:08 -0400

    created test for generateHTML method

--- exercise43/src/test/java/baseline/Solution43Test.java
+++ exercise43/src/test/java/baseline/Solution43Test.java
@@ -4,13 +4,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.io.IOException;
 
 class Solution43Test {
 
     @Test
-    void generateHTML(){}
+    void generateHTML(){
+
+        try {
+            File html = new File("/Users/bendecidos/Documents/COP3330/Code/exercise43/data/website/awesomeco/index.html");
+
+            if(html.createNewFile()){
+                System.out.println("Created ./website/awesomeco/index.html");
+            }
+            else{
+                System.out.println("Nope");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     @Test
     void createFolders(){
@@ -19,12 +34,19 @@ class Solution43Test {
         String folder1 = "/js";
         String folder2 = "/css";
 
-        String path = "./website/awesomeco";
+        String path = "./data/website";
+        String siteName = "/awesomeco";
 
         File f = new File(path);
         boolean bool = f.mkdir();
         if(bool){
             System.out.println("folder is created");
+        }
+        path = path + siteName;
+        File fA = new File(path);
+        bool = fA.mkdir();
+        if(bool){
+            System.out.println("Created ./website/awesomeco");
         }
 
         String path1 = path+folder1;
@@ -34,21 +56,21 @@ class Solution43Test {
         boolean bool2 = f1.mkdir();
         boolean bool3 = f2.mkdir();
         if(bool2){
-            System.out.println("folder is created");
+            System.out.println("Created ./website/awesomeco/js/");
         }
         else{
             System.out.println("Error Found!");
         }
 
         if(bool3){
-            System.out.println("folder is created");
+            System.out.println("Created ./website/awesomeco/css/");
         }
         else{
             System.out.println("Error Found!");
         }
 
-            boolean check = new File("./website/awesomeco", "css").exists();
-            boolean check2 = new File("./website/awesomeco", "js").exists();
+            boolean check = new File("data/website/awesomeco", "css").exists();
+            boolean check2 = new File("data/website/awesomeco", "js").exists();
 
 
         Assertions.assertTrue(check);
