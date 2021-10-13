a428df3b9ed1e0cf950bd2b179711fd0507bc80e
J-Caques <j.caques@knights.ucf.edu>
2021-10-12 22:35:27 -0400

    Initial pseudo code

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,16 @@
+package baseline;
+
+/*
+   Read product name, along w/ price & quantity, from file "exercise44_input.json"
+   Prompt user:
+    "What is the product name?"
+        If product not found:
+            Prompt user again for the product
+   Once found, output:
+    "Name: productName"
+    "Price: productPrice"
+    "Quantity: productQuantity"
+ */
+
+public class Solution44 {
+}
