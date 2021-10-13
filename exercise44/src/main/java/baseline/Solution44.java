188ddfde76cc507d85387f31b277d56bc23f0c2b
J-Caques <j.caques@knights.ucf.edu>
2021-10-12 22:54:54 -0400

    plantUML diagram

--- exercise44/src/main/java/baseline/Solution44.java
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -1,11 +1,17 @@
 package baseline;
 
 /*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Caques
+ */
+
+/*
    Read product name, along w/ price & quantity, from file "exercise44_input.json"
    Prompt user:
     "What is the product name?"
         If product not found:
-            Prompt user again for the product
+            "Sorry that product was not found in our inventory."
+             Prompt user again for the product
    Once found, output:
     "Name: productName"
     "Price: productPrice"
@@ -13,4 +19,5 @@ package baseline;
  */
 
 public class Solution44 {
+
 }
