54285ed07293eeb6cef4cdea35ac6319744bfdaa
J-Caques <89541215+J-Caques@users.noreply.github.com>
2021-10-14 14:51:48 -0400

    intial pseudo code

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,19 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Caques
+ */
+
+/*
+    Read file "exercise45_input.txt"
+    Search for occurrences for the word "utilize"
+    Replace each occurrence of "utilize" w/ "use".
+    Prompt User for name of output file
+    Create output file
+    Place modified file into new output file
+ */
+
+public class Solution45 {
+
+}
