9dc1cd46340492f4268db6a96192ac8ac64122d6
J-Caques <89541215+J-Caques@users.noreply.github.com>
2021-10-14 22:05:41 -0400

    Initial pseudo code

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,11 @@
+package baseline;
+
+/*
+Read file exercise46_input.txt
+Count the frequency of each word in the file.
+Construct a histogram displaying the words and the frequency
+    -Ensure the most used word is at the top; the least at the bottom.
+ */
+
+public class Solution46 {
+}
